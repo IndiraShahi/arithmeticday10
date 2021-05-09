5a56672fe4ade12d8a11fdcfa04c4f7bfbd7f03c
IndiraShahi <IndiraShahi144@gmail.com>
2021-05-09 10:53:28 +0530

    2nd calculation added

--- day10.sh
+++ day10.sh
@@ -4,3 +4,5 @@ read -p "Enter 2nd num: " b
 read -p "Enter 3rd num: " c
 p=$(echo $a $b $c | awk '{print $1+$2*$3}')
 echo "First Number: $p"
+q=$(echo $a $b $c | awk '{print $3+$1/$2}')
+echo "Second Number: $q"
